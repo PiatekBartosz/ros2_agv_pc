8f123422d7b3708f4b00a59c4017e86211fbc146
bartek <bartekpiatek123@gmail.com>
2024-01-25 16:10:39 +0100

    qt: changed to 5.12.2, working on qml hello world app

--- src/cpp_qt_app/src/main.cpp
+++ src/cpp_qt_app/src/main.cpp
@@ -1,4 +1,16 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
 int main(int argc, char *argv[])
 {
-    return 0;
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling); // Enable High DPI scaling
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/src/main.qml"))); // Load main.qml
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
 }
